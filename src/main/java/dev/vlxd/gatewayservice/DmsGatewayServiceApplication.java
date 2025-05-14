80a4f0dd90dd4e00874e5c7f12397f514b3ee28a
Uladzislau Lailo <lailo.vlad@gmail.com>
2025-05-15 01:06:47 +0300

    Initial commit

---
+++ src/main/java/dev/vlxd/gatewayservice/DmsGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.vlxd.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmsGatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DmsGatewayServiceApplication.class, args);
+    }
+
+}
